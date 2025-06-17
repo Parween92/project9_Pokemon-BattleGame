2c1d3943ca0ef9b723cd681f287e5c52345a458c
parween92 <parweena46@gmail.com>
2025-06-17 16:01:01 +0200

    Replaced z.string().url() with z.url()

--- zod-schemas/pokemonSchema.js
+++ zod-schemas/pokemonSchema.js
@@ -3,7 +3,7 @@ import { z } from "zod/v4";
 const pokemonSchema = z.object({
   name: z.string().min(1, "Name darf nicht leer sein").max(50, "Name zu lang"),
 
-  sprite: z.string().url("Sprite muss eine gültige URL sein"),
+  sprite: z.url("Sprite muss eine gültige URL sein"),
 
   maxHp: z.number().min(1, "maxHp muss mindestens 1 sein"),
 
